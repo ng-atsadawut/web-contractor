842b6551c45b9471c61e73848408db13667d1f59
programming-bermatic <personnel.scbs.code@gmail.com>
2021-02-05 15:16:17 +0700

    no message

--- resources/views/fragment/topmenu.blade.php
+++ resources/views/fragment/topmenu.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar">
-    <a href="#" class="active">Home</a>
+    <a href="{{ url('/home') }}" class="active">Home</a>
     <a href="#">Link</a>
     <a href="#">Aboust</a>
     <a href="{{ url('contact') }}" class="right">Contact</a>
